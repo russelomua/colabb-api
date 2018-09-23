868594adc8788859e40d27f3f3a8b973f23f0b22
Nikolaj Klyashtornyj <russel.omua@gmail.com>
2018-09-24 02:52:54 +0300

    tune mongo connection

--- server.js
+++ server.js
@@ -8,12 +8,22 @@ import staffRouter from './routes/staff';
 
 import CrossDomain from './routes/crossDomain'
 
-dotenv.config();
+const env = dotenv.config({path: '.env.local'});
+
+if (env.error) {
+  throw env.error
+}
+console.log('ENV LOADED:', env.parsed);
 
 const app = express();
 const port = process.env.PORT || 5656;
 // Connecting to the database
-const db = mongoose.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}`);
+const dbAddr = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}`;
+
+mongoose.set('useCreateIndex', true);
+const db = mongoose.connect(dbAddr, { useNewUrlParser: true })
+  .then(res => console.log("CONNECTED to MongoDB"))
+  .catch((err) => console.log("CAN'T CONNECT to MongoDB"));
 
 // setting body parser middleware 
 app.use(bodyParser.json());
@@ -30,5 +40,6 @@ app.use('/api/staff', staffRouter);
 
 // Running the server
 app.listen(port, () => {
-	console.log(`http://localhost:${port}`)
+  console.log(`API RUNS ON: http://localhost:${port}`);
 })
+
